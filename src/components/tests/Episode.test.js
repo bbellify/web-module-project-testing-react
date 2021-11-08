1b293476f495f06d7bcaf8622faaa1a8488202f7
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-11-08 15:46:35 -0800

    first test suite passing

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,23 +8,41 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "Summary statement test 12345",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "Summary statement test 12345",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode={testEpisode}/>)
 });
 
 test("renders the summury test passed as prop", ()=>{
-    
+    render(<Episode episode={testEpisode} />)
+
+    const summary = screen.queryByText(/summary statement test 12345/i);
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent(/12345/i)
 });
 
 test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={testEpisodeWithoutImage} />);
+
+    const noImage = screen.getByAltText('./stranger_things.png');
+
+    expect(noImage).toBeInTheDocument();
+
     
 })
 
